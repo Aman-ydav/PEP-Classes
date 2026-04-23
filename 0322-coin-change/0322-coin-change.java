c8fba903e6de23dbd16cc10ccb7752be0dbc51f7
Aman <137027542+Aman-ydav@users.noreply.github.com>
2026-04-23 15:08:06 +0530

    Time: 39 ms (9.08%), Space: 48.6 MB (5.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,31 +1,31 @@
 class Solution {
-    Integer[][] dp;
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
+        Integer[][] dp = new Integer[n+1][amount+1];
 
-        dp = new Integer[amount+1][n+1];
-        int a =  solve(coins,amount,n-1);
-        return a==Integer.MAX_VALUE ? -1 : a;
+        int a = solve(coins,dp,amount,n-1);
+
+        return a==Integer.MAX_VALUE ? -1 :a;
     }
-    int solve(int[] arr,int sum,int n){
-
-        if(sum==0){
+    int solve(int[] coins,Integer[][] dp,int amount,int i){
+        if(amount==0){
             return 0;
         }
-        
-        if(n<0 || sum<0){
+        if(i<0 || amount<0){
             return Integer.MAX_VALUE;
         }
+        if(dp[i][amount]!=null) return dp[i][amount]; 
 
-        if(dp[sum][n]!=null) return dp[sum][n];
-        int take = solve(arr,sum-arr[n],n);
+        int skip = solve(coins,dp,amount,i-1);
 
-        if(take!=Integer.MAX_VALUE){
-            take +=1;
-        }
-        int skip = solve(arr,sum,n-1);
+        int take = 0;
 
-        return  dp[sum][n]= Math.min(take,skip);
+            take = solve(coins,dp,amount-coins[i],i);
         
+        if(take!=Integer.MAX_VALUE){
+            take+=1;
+        }
+
+        return dp[i][amount] = Math.min(take,skip);
     }
 }
