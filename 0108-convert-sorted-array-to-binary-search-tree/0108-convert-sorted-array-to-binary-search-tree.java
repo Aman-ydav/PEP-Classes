bcdf3b83be240ee76f235f708989add772e05cfd
Aman <137027542+Aman-ydav@users.noreply.github.com>
2026-06-17 11:59:33 +0530

    Time: 0 ms (100%), Space: 45.1 MB (44.34%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -15,19 +15,16 @@
  */
 class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
-        int n = nums.length;
-        if(n==0) return null;
-        return inorder(nums,0,n-1);
+        return build(nums,0,nums.length-1);
     }
-    TreeNode inorder(int[] nums,int i,int j){
-    
+    public TreeNode build(int[] nums,int i,int j){
         if(i>j) return null;
         int mid = i+(j-i)/2;
 
         TreeNode root = new TreeNode(nums[mid]);
-        
-        root.left = inorder(nums,i,mid-1);
-        root.right = inorder(nums,mid+1,j);
+
+        root.left = build(nums,i,mid-1);
+        root.right = build(nums,mid+1,j);
 
         return root;
     }
