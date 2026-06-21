39963d8b7540edd7e010c53da6e2564c3f665696
Aman <137027542+Aman-ydav@users.noreply.github.com>
2026-06-21 16:46:00 +0530

    Time: 36 ms (77.33%), Space: 84.2 MB (15.99%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int n = costs.length;
+        int sum =0;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            sum += costs[i];
+            if(sum<=coins){
+                count++;
+            }
+            else break;
+        }
+        return count;
+    }
+}
