63315f959648ee2c9a6bb07cb5391a53cdbc30d2
Aman <137027542+Aman-ydav@users.noreply.github.com>
2026-06-21 14:05:38 +0530

    Time: 53 ms (86.11%), Space: 125.8 MB (19.44%) - LeetHub

--- 1840-maximum-building-height/1840-maximum-building-height.java
+++ 1840-maximum-building-height/1840-maximum-building-height.java
@@ -11,7 +11,7 @@ class Solution {
         arr[0][0] = 1;
         arr[0][1] = 0;
 
-        for (int i = 0; i < m; i++) {
+        for (int i= 0; i < m; i++) {
             arr[i + 1] = restrictions[i];
         }
 
